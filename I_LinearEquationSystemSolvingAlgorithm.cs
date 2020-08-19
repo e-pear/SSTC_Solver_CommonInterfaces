b06360a9d7d6045781085ce0c8ede777244726b0
e-Pear <HomeFortress>
2020-08-19 15:57:03 +0200

    Add project files.

---
+++ I_LinearEquationSystemSolvingAlgorithm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SSTC_Solver_CommonInterfaces
+{
+    public interface I_LinearEquationSystemSolvingAlgorithm
+    {
+        /// <summary>
+        /// Solves linear equations system: AX = B.
+        /// </summary>
+        /// <param name="A">Coefficient Array.</param>
+        /// <param name="B">Vector of corresponding free parameters.</param>
+        /// <param name="X">Vector of solutions.</param>
+        /// <returns>True if calculations succeeded.</returns>
+        bool Solve_LinearEquationSystem(double[,] A, double[] B, ref double[] X);
+        /// <summary>
+        /// Algorithm name property.
+        /// </summary>
+        string LabelName { get; }
+        /// <summary>
+        /// Algorithm short description property.
+        /// </summary>
+        string Description { get; }
+    }
+}
---
+++ I_NewtonRaphson_SolveableModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSTC_Solver_CommonInterfaces
+{
+    //
+    public interface I_NewtonRaphson_SolveableModel
+    {
+        int VectorBaseSize { get; }
+        double[] Get_Vector_F(double[] initialSolutionsVector_X);
+        double[,] Get_Jacobian_J(double[] initialSsolutionsVector_X);
+    }
+}
